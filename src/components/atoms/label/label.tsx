931d52995232fe42cb51821c516f850d824cccc0
kapiltaneja8 <kapiltaneja8@gmail.com>
2021-12-27 21:31:49 +0530

    adding newer config changes

---
+++ src/components/atoms/label/label.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const Label = (value:string, className: string) => {
+    return <label className={className}>{value}</label>
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,30 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  mode: 'development',
+  devtool: 'source-map',
   module: {
     rules: [
+      
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: ["babel-loader"]
+      // },
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ["babel-loader"]
-      },
+        test: /\.(ts|tsx)$/,
+        loader: "awesome-typescript-loader",
+      },  
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      }
     ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
+    extensions: ["*", ".js", ".jsx", ".json", ".ts", ".tsx"],
   },
   output: {
     path: path.resolve(__dirname, "dist/"),
